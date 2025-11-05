b997640b2f4f088ff1eee82fadc77f6c79770247
suhana kousar <163721735+suhanakousar@users.noreply.github.com>
2025-11-05 13:03:58 +0530

    fasgjssaab

--- src/auth/RequireAuth.jsx
+++ src/auth/RequireAuth.jsx
@@ -1,7 +1,7 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 export const RequireAuth = () => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem("authToken");
   const location = useLocation();
 
   return token ? <Outlet /> : <Navigate to={`/Signin?redirect=${encodeURIComponent(location.pathname)}`} replace />;
